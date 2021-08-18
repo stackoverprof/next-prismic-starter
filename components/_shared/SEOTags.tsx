81b2d2c504246f8ed92e5cf63ea00f862846f5ea
stackoverprof <bintangbagus01@mail.ugm.ac.id>
2021-08-19 06:55:20 +0700

    new update on extras

--- components/_shared/SEOTags.tsx
+++ components/_shared/SEOTags.tsx
@@ -1,38 +1,45 @@
+
+import { useRouter } from 'next/dist/client/router';
+import Head from 'next/head';
 import React from 'react';
 
-const SEOTags = (): JSX.Element => (
-	<>
-		<link rel="canonical" href="[DOMAIN]" />
-		<meta name="description" content="[DESCRIPTION]" />
-		<meta name="summary" content=""/>
-		<meta name="keywords" content=""/>
-		<meta name="locale" content="en"/>
-		<meta name="robots" content="index,follow"/>
-		<meta name="googlebot" content="index,follow"/>
-		<meta name="copyright"content=""/>
-		<meta name="subject" content="[PROJECT_NAME]"/>
-		<meta name="revised" content="" />
-		<meta name="abstract" content=""/>
-		<meta name="topic" content="[CATEGORY]"/>
-		<meta name="Classification" content="[CATEGORY]"/>
-		<meta name="author" content="[AUTHOR], [EMAIL]"/>
-		<meta name="designer" content=""/>
-		<meta name="reply-to" content="[EMAIL]"/>
-		<meta name="owner" content="[PROJECT_NAME]"/>
-		<meta name="url" content="[DOMAIN]"/>
-		<meta name="identifier-URL" content="[DOMAIN]"/>
-		<meta name="directory" content="submission"/>
-		<meta name="category" content="[CATEGORY]"/>
-		<meta name="coverage" content="Worldwide"/>
-		<meta name="distribution" content="Global"/>
-		<meta name="rating" content="General"/>
-		
-		<meta property="og:site_name" content="[PROJECT_NAME]" />
-		<meta property="og:title" content="[PROJECT_NAME]" />
-		<meta property="og:url" content="[DOMAIN]" />
-		<meta property="og:description" content="[DESCRIPTION]" />
-		<meta property="og:image" content="" />
-	</>
-);
+const DEFAULT = {
+	sitename: 'My Site',
+	domain: 'http://my-site.com/',
+	description: 'Site description that contains some information about what is your site about and blablabla',
+	image: 'https://images.unsplash.com/photo-1629058622223-93665bf5d046?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+};
+
+
+const SEOTags = (props: any) => {
+	const data = {...DEFAULT, ...props};
+
+	const router = useRouter();
+
+	const supertitle = data.title ? `${data.title} — ${data.sitename} ` : data.sitename;
+	
+	return (
+		<Head>
+			<title>{supertitle}</title>
+
+			<meta name="robots" content="follow, index" />
+			<meta name="description" content={data.description} />
+			<meta property="og:url" content={router.asPath} />
+			<link rel="canonical" href={router.asPath} />
+			
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content={supertitle} />
+			<meta property="og:site_name" content={supertitle} />
+			<meta property="og:description" content={data.description} />
+			<meta property="og:image" name="image" content={data.image} />
+
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:site" content={supertitle} />
+			<meta name="twitter:title" content={supertitle} />
+			<meta name="twitter:description" content={data.description} />
+			<meta name="twitter:image" content={data.image} />
+		</Head>
+	);
+};
 
 export default SEOTags;
